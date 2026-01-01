a7a6db87ba1fac799eef6e3aed57496fccbcf535
kshitiz <kshitizpaudel5@gmail.com>
2026-01-01 21:48:34 +0545

    route table and subnet association done

--- infra/networking/main.tf
+++ infra/networking/main.tf
@@ -4,10 +4,8 @@ variable "cidr_public_subnet"{}
 variable "cidr_private_subnet"{}  
 variable "availability_zone"{}
 
-
 resource "aws_vpc" "dev_proj_1_eu_central_1"{
     cidr_block = var.vpc_cidr
-
     tags = {
         Name = var.vpc_name
     }
@@ -27,8 +25,6 @@ tags = {
 
 }
 }
-
-
 
 
 ## setting up the public subnet
@@ -53,4 +49,46 @@ vpc_id = aws_vpc.dev_proj_1_eu_central_1.id
 tags = {
     Name: "dev-project-1-igw"
 }
+}
+
+#Setup the route table for the public subnet
+resource "aws_route_table" "dev_project_1_public_route"{
+    vpc_id = aws_vpc.dev_proj_1_eu_central_1.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.dev_project_1_igw.id
+    }
+
+    tags = {
+        Name = "dev-proj-1-public-rt"
+    }
+}
+
+
+# Setup association between public rt and subnet
+resource "aws_route_table_association" "dev_proj_1_public_route_table_association"{
+
+    count = length(aws_subnet.dev_proj_1_public_subnets)
+    subnet_id = aws_subnet.dev_proj_1_public_subnets[count.index].id
+    route_table_id = aws_route_table.dev_project_1_public_route.id
+}
+
+
+#Route table for Private Subnet
+
+resource "aws_route_table" "dev_proj_1_private_route_table"{
+vpc_id = aws_vpc.dev_proj_1_eu_central_1.id
+
+tags = {
+    Name = "dev-proj-1-private-rt"
+}
+}
+
+
+#Private Route Table and private subnet association
+resource "aws_route_table_association" "dev_proj_1_private_route_table_association"{
+
+    count = length(aws_subnet.dev_proj_1_private_subnets)
+    subnet_id = aws_subnet.dev_proj_1_private_subnets[count.index].id
+    route_table_id = aws_route_table.dev_proj_1_private_route_table.id
 }
